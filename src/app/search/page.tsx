dc51d40c9637a9fafe14c8c35c330fa4785ed492
IceBear9028 <tlatms9028@gmail.com>
2025-02-22 15:10:32 +0900

    [Fix] : PageRouter props 수정
    ### 문제원인
    - Next 15 로 업데이트 되면서 `searchParams` 를 호출할 때 비동기로 호출해야 한다.
    - [Next15 Docs](https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#examples)
    
    ### 해결방안
    - 문제가 발생한 `SearchPage` 의 props 를 변경, `searchParams` 의 타입을 Promise 로 감싸서 해결

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,7 @@ import { SearchResponse } from '@/app/api/search/route';
 import { url } from '@/constant/url';
 import { Search } from '@/components/search';
 import { Table } from '@/components/ui/table';
+import { Fragment } from 'react';
 
 /** API 요청 함수: 검색어가 없으면 요청하지 않음 */
 async function fetchGetSearchResult(searchText?: string): Promise<SearchResponse | null> {
@@ -18,35 +19,40 @@ async function fetchGetSearchResult(searchText?: string): Promise<SearchResponse
   }
 }
 
-/** 검색 페이지 컴포넌트 */
-export default async function SearchPage({ searchParams }: { searchParams?: { search_text?: string } }) {
-  const searchText = searchParams?.search_text || '';
-  const data = await fetchGetSearchResult(searchText);
+interface SearchPageProps {
+  searchParams: Promise<{ search_text: string }>;
+}
 
+/** 검색 페이지 컴포넌트 */
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const params = await searchParams;
+  const data = await fetchGetSearchResult(params.search_text);
   return (
     <>
-      <Search value={searchText} />
+      <Search value={params.search_text} />
       <article>
         {data && data.result.length > 0 ? (
           <section>
-            <Table.Header text="기본정보" />
             {data.result.map((item) => (
-              <table className="w-full" key={item.ADDR}>
-                <tbody className="flex flex-col">
-                  <Table.Tr>
-                    <Table.Th>업소명</Table.Th>
-                    <Table.Td>{item.PRCSCITYPOINT_BSSHNM}</Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Th>주소</Table.Th>
-                    <Table.Td>{item.ADDR}</Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Th>행정처분</Table.Th>
-                    <Table.Td>{item.DSPSCN}</Table.Td>
-                  </Table.Tr>
-                </tbody>
-              </table>
+              <Fragment key={item.ADDR}>
+                <Table.Header text="기본정보" />
+                <table className="w-full">
+                  <tbody className="flex flex-col">
+                    <Table.Tr>
+                      <Table.Th>업소명</Table.Th>
+                      <Table.Td>{item.PRCSCITYPOINT_BSSHNM}</Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Th>주소</Table.Th>
+                      <Table.Td>{item.ADDR}</Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Th>행정처분</Table.Th>
+                      <Table.Td>{item.DSPSCN}</Table.Td>
+                    </Table.Tr>
+                  </tbody>
+                </table>
+              </Fragment>
             ))}
           </section>
         ) : (
